7592109a7ce85c8e7effed40836ce40936c74430
savaleks <38282726+savaleks@users.noreply.github.com>
2018-06-11 15:11:45 +0300

    Update perfect.java

--- src/lesson2/perfect.java
+++ src/lesson2/perfect.java
@@ -1,12 +1,14 @@
 package lesson2;
-
+//rasti tobula skaiciu duotam intervale;
 public class perfect {
     public static void main(String[] args){
+        //intervalas nuo 1 iki 1000
         int min = 1;
         int max = 1000;
 
         for (min = 1; min <= max; min++) {
             int sum = 0;
+            //dalina ir lygina skaicius
             for (int e = 1; e < min; e++) {
                 if ((min % e) == 0) {
                     sum += e;
